c0d89c24c36847f583b7a70a7d8a4554347eba26
getty104 <hayabusatoshihumi@gmail.com>
2025-03-05 23:19:15 +0900

    Change llm plugin to avante.nvim

--- nvim/lua/lazy-nvim/plugins.lua
+++ nvim/lua/lazy-nvim/plugins.lua
@@ -94,33 +94,73 @@ return {
     end,
   },
   {
-    "olimorris/codecompanion.nvim",
+    "yetone/avante.nvim",
+    event = "VeryLazy",
     lazy = false,
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+
     keys = {
-      { "<C-n>", "<Cmd>CodeCompanionChat Toggle<CR>" },
-      { "<C-a>", "<Cmd>CodeCompanionActions<CR>" },
+      { "<C-n>", "<Cmd>AvanteToggle<CR>" },
+      { "<C-a>", "<Cmd>AvanteAsk<CR>" },
     },
-    config = function()
-      require("codecompanion").setup({
-        strategies = {
-          chat = {
-            adapter = "copilot",
-          },
-          inline = {
-            adapter = "copilot",
-          },
-          agent = {
-            adapter = "copilot",
-          },
-        },
-        opts = {
-          language = "Japanese",
-          log_level = "INFO",
-        },
-      })
-    end,
+
+    opts = {
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
+
+      copilot = {
+        model = "claude-3.5-sonnet",
+      },
+
+      behaviour = {
+        auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = true,
+        minimize_diff = true,
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "Avante" },
+        },
+        ft = { "Avante" },
+      },
     },
   },
   {
