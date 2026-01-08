ab91ecb13fe32a97e708460c035d7a67bf8765b3
getty104 <hayabusatoshihumi@gmail.com>
2026-01-08 21:54:53 +0900

    Update hammerspoon init.lua and wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,8 +12,8 @@ config.scrollback_lines = 10000
 config.use_ime = true
 config.window_background_opacity = 0.6
 config.font = wezterm.font_with_fallback({
-	{ family = "Hack Nerd Font Mono", weight = "Bold" },
-	{ family = "Hiragino Sans", weight = "Bold" },
+  { family = "Hack Nerd Font Mono", weight = "Bold" },
+  { family = "Hiragino Sans", weight = "DemiBold" },
 })
 
 config.adjust_window_size_when_changing_font_size = true
@@ -23,9 +23,9 @@ config.hyperlink_rules = wezterm.default_hyperlink_rules()
 -- Start in fullscreen mode
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(window)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	local gui_window = window:gui_window()
-	gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  local gui_window = window:gui_window()
+  gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
 end)
 
 return config
