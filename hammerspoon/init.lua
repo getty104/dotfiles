9a1c1d3397bde3bdf93866b9a862da1927604ffe
getty104 <hayabusatoshihumi@gmail.com>
2026-03-21 16:09:55 +0900

    fix: Remove Ctrl+h/l keyboard shortcuts from Hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,19 +12,3 @@ local open_wezterm = function()
 end
 -- 「Ctrl+t」のショートカットで表示／非表示を切り替える場合の設定
 hs.hotkey.bind({ "ctrl" }, "j", open_wezterm)
-
-hs.hotkey.bind({ "ctrl" }, "h", function()
-  local win = hs.window.focusedWindow()
-  if win then
-    local screen = win:screen():toWest()
-    if screen then win:moveToScreen(screen) end
-  end
-end)
-
-hs.hotkey.bind({ "ctrl" }, "l", function()
-  local win = hs.window.focusedWindow()
-  if win then
-    local screen = win:screen():toEast()
-    if screen then win:moveToScreen(screen) end
-  end
-end)
